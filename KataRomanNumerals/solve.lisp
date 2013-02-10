0062c4d82cd7f72a21bf38f3beb0be73a7ff544b
Johan Lindberg <johanlindberg@sofia-lindbergs-macbook-pro.local>
2013-02-10 21:51:34 +0100

    Switched to doctest and added implementation for failing test.
    This implementation is obviously too simple to work. Next I'll add a
    test for 2 -> II.

--- KataRomanNumerals/solve.lisp
+++ KataRomanNumerals/solve.lisp
@@ -1,11 +1,15 @@
 ;;;; Roman Numerals
 ;;;; --------------
 ;;;;
-;;;; > (load "/path/to/solve.lisp")
-;;;; > (lisp-unit:run-tests '(tests))
-
-;; Tests
-(lisp-unit:define-test tests
-  (lisp-unit:assert-equal "I" (roman-numeral 1)))
+;;;; > (load "/Users/johanlindberg/Projects/doctest/doctest.lisp")
+;;;; > (use-package :doctest)
+;;;; > (test-function #'roman-numeral :output T)
 
 ;; Implementation
+(defun roman-numeral (n)
+  "Returns a string with the Roman numeral representing n.
+
+  >> (roman-numeral 1)
+  \"I\"
+  "
+  "I")
