b12f14f56b5323a9509e217ce4530be2580ee712
Johan Lindberg <johan@pulp.se>
2013-02-11 13:20:48 +0100

    More tests. Up to 39 works fine and 40 fails (as was expected).

--- KataRomanNumerals/solve.lisp
+++ KataRomanNumerals/solve.lisp
@@ -15,14 +15,17 @@
   >> (mapcar #'roman-numeral '(11 12 13 14 15 16 17 18 19 20))
   (\"XI\" \"XII\" \"XIII\" \"XIV\" \"XV\" \"XVI\" \"XVII\" \"XVIII\" \"XIX\" \"XX\")
 
-  >> (mapcar #'roman-numeral '(21 22 23 24 25 26 27 28 29))
-  (\"XXI\" \"XXII\" \"XXIII\" \"XXIV\" \"XXV\" \"XXVI\" \"XXVII\" \"XXVIII\" \"XXIX\")
+  >> (mapcar #'roman-numeral '(21 22 23 24 25 26 27 28 29 30))
+  (\"XXI\" \"XXII\" \"XXIII\" \"XXIV\" \"XXV\" \"XXVI\" \"XXVII\" \"XXVIII\" \"XXIX\" \"XXX\")
 
   I'm expecting this test to fail which is why it's not part of the mapcar-bulk
   of tests.
 
-  >> (roman-numeral 30)
-  \"XXX\"
+  >> (mapcar #'roman-numeral '(31 32 33 34 35 36 37 38 39))
+  (\"XXXI\" \"XXXII\" \"XXXIII\" \"XXXIV\" \"XXXV\" \"XXXVI\" \"XXXVII\" \"XXXVIII\" \"XXXIX\")
+
+  >> (roman-numeral 40)
+  \"XL\"
   "
   (do ((result "")
        (symbols '("X" "X" "X" "V")) ; Roman symbols and
