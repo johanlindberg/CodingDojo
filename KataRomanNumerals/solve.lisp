2f3af7171edd12d08c5d928204d00cd0a5e34510
Johan Lindberg <johan@pulp.se>
2013-02-11 18:21:21 +0100

    Removed some tests and parts of the implementation.
    Let's take a few steps back to (roughly) were the implementation
    could manage all numbers up to 10. That's whre I started taking
    shortcuts in the implementation.
    
    Now that I know more about how this implementation is going to
    turn out I can start the actual refactoring.
    
    The first thing that needs to happen is to break this into two
    functions. The function roman-numeral (as it is now) will be
    doing the lion share of the work by producing roman numeral
    patterns (like VII). The other function will split the number
    into parts and provide the inner function with which symbols it
    should fill the pattern with. XVI for up to ten, CLX for up to
    100 and MDC for up to 1000.

--- KataRomanNumerals/solve.lisp
+++ KataRomanNumerals/solve.lisp
@@ -11,22 +11,10 @@
 
   >> (mapcar #'roman-numeral '(1 2 3 5 6 7 8 4 10 9))
   (\"I\" \"II\" \"III\" \"V\" \"VI\" \"VII\" \"VIII\" \"IV\" \"X\" \"IX\")
-
-  >> (mapcar #'roman-numeral '(11 12 13 14 15 16 17 18 19 20))
-  (\"XI\" \"XII\" \"XIII\" \"XIV\" \"XV\" \"XVI\" \"XVII\" \"XVIII\" \"XIX\" \"XX\")
-
-  >> (mapcar #'roman-numeral '(21 22 23 24 25 26 27 28 29 30))
-  (\"XXI\" \"XXII\" \"XXIII\" \"XXIV\" \"XXV\" \"XXVI\" \"XXVII\" \"XXVIII\" \"XXIX\" \"XXX\")
-
-  I'm expecting this test to fail which is why it's not part of the mapcar-bulk
-  of tests.
-
-  >> (mapcar #'roman-numeral '(31 32 33 34 35 36 37 38 39))
-  (\"XXXI\" \"XXXII\" \"XXXIII\" \"XXXIV\" \"XXXV\" \"XXXVI\" \"XXXVII\" \"XXXVIII\" \"XXXIX\")
   "
   (do ((result "")
-       (symbols '("X" "X" "X" "V")) ; Roman symbols and
-       (values  '( 10  10  10  5))  ; their values.
+       (symbols '("X" "V")) ; Roman symbols and
+       (values  '( 10  5))  ; their values.
        (n number))
       ((eq n 0) result)
     (let ((v (pop values))
