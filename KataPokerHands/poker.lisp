2cfddc2362c2d492bd11239dfb7831d2a2d51ad6
Johan Lindberg <johan@pulp.se>
2013-02-13 15:09:18 +0100

    Implementation.
    Ok. This commit contains a little refactoring as well. I broke out
    the same-suit implementaion and put in it's own function. At least
    for now. That means I had to rename all the tests as well and that's
    probably cheating.
    
    The function same-suit-p takes any number of cards and returns t if
    they're all in the same suit, otherwise it returns nil. It works by
    looping through each of the cards, checking if they all belong to
    (member) the same suit using the lists hearts and spades.

--- KataPokerHands/poker.lisp
+++ KataPokerHands/poker.lisp
@@ -2,23 +2,31 @@
 ;;;; -----------
 
 (defun rank (card1 card2)
-  "Rank <cards> (using poker rules) and return which is the winner and why.
+  "Rank <cards> (using poker rules) and return which is the winner and why."
+  t)
 
-  >> (rank '2H '3H)
+(defun same-suit-p (&rest cards)
+  "Returns t if all <cards> are of the same suit, otherwise returns nil.
+
+  >> (same-suit-p '2H '3H)
   t
 
-  >> (rank '2H '2S)
+  >> (same-suit-p '2H '2S)
   nil
 
-  >> (rank '2H '3H '4H '5H '6H)
+  >> (same-suit-p '2H '3H '4H '5H '6H)
   t
 
-  >> (rank '2H '3H '4H '5H '6H '2S)
+  >> (same-suit-p '2H '3H '4H '5H '6H '2S)
   nil
   "
-  (let ((hearts '(2H 3H))
-	(spades '(2S)))
+  (let ((hearts '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
+	(spades '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS)))
     (dolist (suit (list hearts spades))
-      (when (and (member card1 suit)
-		 (member card2 suit))
-	(return-from rank t)))))
+      (let ((same-suit t))
+	(dolist (card cards)
+	  (unless (member card suit)
+	    (setq same-suit nil)
+	    (return)))
+	(when same-suit
+	  (return-from same-suit-p t))))))
