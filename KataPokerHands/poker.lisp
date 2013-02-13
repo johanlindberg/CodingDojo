86e3853fe695cb449eef8505b1c390ae92e485b7
Johan Lindberg <johan@pulp.se>
2013-02-13 22:26:07 +0100

    Implementation.
    Moved out the lists of suits to defparameters because I thought
    I would need them in the implementation of high-card. However,
    having finished that implementation now, it turned out I didn't.
    I added another parameter instead called values.
    
    The implementations is based around a sorted list of all cards
    and each card has a value that is equal to the length of the list
    returned by (member card *values*). The lower the better (at
    least for now).

--- KataPokerHands/poker.lisp
+++ KataPokerHands/poker.lisp
@@ -1,6 +1,17 @@
 ;;;; Poker Hands
 ;;;; -----------
 
+
+(defparameter *clubs*    '(2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC))
+(defparameter *diamonds* '(2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD))
+(defparameter *hearts*   '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
+(defparameter *spades*   '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS))
+
+(defparameter *values*   '(2C 2D 2H 2S 3C 3D 3H 3S 4C 4D 4H 4S 5C 5D 5H 5S
+			   6C 6D 6H 6S 7C 7D 7H 7S 8C 8D 8H 8S 9C 9D 9H 9S
+			   TC TD TH TS JC JD JH JS QC QD QH QS KC KD KH KS
+			   AC AD AH AS))
+
 (defun rank (hand)
   "Rank <hand> (using poker rules) and returns the score and a description."
   t)
@@ -17,18 +28,14 @@
   >> (same-suit-p '(2D 3D)) t
 
   "
-  (let ((clubs    '(2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC))
-	(diamonds '(2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD))
-	(hearts   '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
-	(spades   '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS)))
-    (dolist (suit (list clubs diamonds hearts spades))
-      (let ((same-suit t))
-	(dolist (card hand)
-	  (unless (member card suit)
-	    (setq same-suit nil)
-	    (return)))
-	(when same-suit
-	  (return-from same-suit-p t))))))
+  (dolist (suit (list *clubs* *diamonds* *hearts* *spades*))
+    (let ((same-suit t))
+      (dolist (card hand)
+	(unless (member card suit)
+	  (setq same-suit nil)
+	  (return)))
+      (when same-suit
+	(return-from same-suit-p t)))))
 
 (defun flush-p (hand)
   "Returns t if all 5 cards in <hand> are from the same suit.
@@ -48,4 +55,9 @@
   >> (high-card '(2S 3S))
   3S
   "
-  nil)
+  (let ((highest-card '2C))
+    (dolist (card hand)
+      (when (< (length (member card *values*))
+	       (length (member highest-card *values*)))
+	(setq highest-card card)))
+    highest-card))
