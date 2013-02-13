67729579da43671c5ca23fcb938c484a435fb99f
Johan Lindberg <johan@pulp.se>
2013-02-13 16:35:16 +0100

    Implementation.
    Added data for the other suits as well. This data should probably
    be a defparameter or something instead since it will be shared
    with a lot of other functions as well.

--- KataPokerHands/poker.lisp
+++ KataPokerHands/poker.lisp
@@ -17,9 +17,11 @@
   >> (same-suit-p '2D '3D) t
 
   "
-  (let ((hearts '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
-	(spades '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS)))
-    (dolist (suit (list hearts spades))
+  (let ((clubs    '(2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC))
+	(diamonds '(2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD))
+	(hearts   '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
+	(spades   '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS)))
+    (dolist (suit (list clubs diamonds hearts spades))
       (let ((same-suit t))
 	(dolist (card cards)
 	  (unless (member card suit)
