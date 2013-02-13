1932ec532f5277e4e88b8bc826ac340081342a2b
Johan Lindberg <johan@pulp.se>
2013-02-13 16:31:08 +0100

    Test.
    Added some more tests to cover also other suits than Hearts and
    Spades.

--- KataPokerHands/poker.lisp
+++ KataPokerHands/poker.lisp
@@ -8,17 +8,14 @@
 (defun same-suit-p (&rest cards)
   "Returns t if all <cards> are of the same suit, otherwise returns nil.
 
-  >> (same-suit-p '2H '3H)
-  t
+  >> (same-suit-p '2H '3H) t
+  >> (same-suit-p '2H '2S) nil
+  >> (same-suit-p '2H '3H '4H '5H '6H) t
+  >> (same-suit-p '2H '3H '4H '5H '6H '2S) nil
+  >> (same-suit-p '2S '2H '3H '4H '5H '6H) nil
+  >> (same-suit-p '2S '2D '2H '2C) nil
+  >> (same-suit-p '2D '3D) t
 
-  >> (same-suit-p '2H '2S)
-  nil
-
-  >> (same-suit-p '2H '3H '4H '5H '6H)
-  t
-
-  >> (same-suit-p '2H '3H '4H '5H '6H '2S)
-  nil
   "
   (let ((hearts '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
 	(spades '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS)))
