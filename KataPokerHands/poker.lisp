cf7965a2b53a4a9b6172ceff737bd00dbd442e9b
Johan Lindberg <johan@pulp.se>
2013-02-17 21:38:02 +0100

    Refactoring.
    Got rid of the defparameter *values*. That simplified the score function
    somewhat and got rid of the repeat definition of the deck of cards but it
    also meant I had to add a function (suit) to figure out which defparmeter
    to use for score calculation.

--- KataPokerHands/poker.lisp
+++ KataPokerHands/poker.lisp
@@ -1,17 +1,10 @@
 ;;;; Poker Hands
 ;;;; -----------
 
-
-(defparameter *clubs*    '(2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AC))
-(defparameter *diamonds* '(2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AD))
-(defparameter *hearts*   '(2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AH))
-(defparameter *spades*   '(2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS AS))
-
-(defparameter *values*   '(AS AH AD AC KS KH KD KC QS QH QD QC
-			   JS JH JD JC TS TH TD TC 9S 9H 9D 9C
-			   8S 8H 8D 8C 7S 7H 7D 7C 6S 6H 6D 6C
-			   5S 5H 5D 5C 4S 4H 4D 4C 3S 3H 3D 3C
-			   2S 2H 2D 2C))
+(defparameter *clubs*    '(AC KC QC JC TC 9C 8C 7C 6C 5C 4C 3C 2C))
+(defparameter *diamonds* '(AD KD QD JD TD 9D 8D 7D 6D 5D 4D 3D 2D))
+(defparameter *hearts*   '(AH KH QH JH TH 9H 8H 7H 6H 5H 4H 3H 2H))
+(defparameter *spades*   '(AS KS QS JS TS 9S 8S 7S 6S 5S 4S 3S 2S))
 
 (defun rank (hand)
   "Rank <hand> (using poker rules) and returns a list with scores.
@@ -259,11 +252,17 @@
 (defun scores (hand)
   (sort (mapcar #'score hand) #'>))
 
+(defun suit (card)
+  "Returns the suit for <card>."
+  (cond ((member card *clubs*) *clubs*)
+        ((member card *diamonds*) *diamonds*)
+        ((member card *hearts*) *hearts*)
+        ((member card *spades*) *spades*)))
+
 (defun score (card)
   "Returns the score for <card>.
   
   >> (mapcar #'score '(2C 3H 4S 5D 6C 7H 8S 9D TC JH QS KD AC))
   (2 3 4 5 6 7 8 9 10 11 12 13 14)
   "
-  (+ 1 (ceiling (/ (length (member card *values*)) 4))))
-
+  (+ 1 (length (member card (suit card)))))
